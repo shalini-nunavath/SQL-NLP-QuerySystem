2570fb8f8fd3fb8873cef119ce327df7ff1fee26
shalini-nunavath <shalini.nunavath@techoptima.in>
2024-07-02 15:42:17 +0530

    Update import_data_to_db.py

--- import_data_to_db.py
+++ import_data_to_db.py
@@ -274,7 +274,7 @@ import matplotlib.pyplot as plt
 import subprocess
 
 # Run the setup.sh script to download the spacy model
-subprocess.run('python', '-m', 'spacy', 'download', 'en_core_web_sm'], check=True)
+subprocess.run(['python', '-m', 'spacy', 'download', 'en_core_web_sm'], check=True)
 
 
 # Load SpaCy model
